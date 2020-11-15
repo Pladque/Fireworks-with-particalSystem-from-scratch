458b408921d249d27f2a66b1064f87fba4504de1
Pladque <63751303+Pladque@users.noreply.github.com>
2020-11-15 21:11:47 +0100

    particles form fire

--- classes.py
+++ classes.py
@@ -1,7 +1,11 @@
+from random import randint
 class Particle:
-    def __init__(self, position, speed, ttl):
+    def __init__(self, position, speed, ttl, size):
         self.position_x = position[0]
         self.position_y = position[1]
         self.speed_x = speed[0]
         self.speed_y = speed[1]
         self.ttl = ttl
+        self.size = size
+        self.velocity_x = randint(-10,10) / 10000
+        self.velocity_y = randint(-10,10) / 10000
--- main.py
+++ main.py
@@ -19,7 +19,9 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT),0,32)
  
 # [loc, velocity, timer]
 particles = []
-    
+PARTICLES_DENSE = 50
+do_it_once = True
+
 year = datetime.datetime.now().year
 hour = datetime.datetime.now().hour
  
@@ -30,20 +32,25 @@ while True:
 
     #if datetime.datetime.now().year != year:
     if True:
-        
-        particles.append(Particle([WIDTH / 2,HEIGHT / 2], [random.randint(-10,10) / 100, random.randint(-10,10) / 100], random.randint(4, 6)))
+        if do_it_once:
+            for _ in range(PARTICLES_DENSE):
+                particles.append(Particle([WIDTH / 2 + random.randint(-10,10),HEIGHT / 2 + random.randint(-10,10)], 
+                [random.randint(-10,10) / 10000, random.randint(-10,10) / 10000], random.randint(10, 18), random.randint(1, 2)))
+            do_it_once = False
+            
         rem = []
         for particle in particles:
             particle.position_x += particle.speed_x
             particle.position_y += particle.speed_y
             particle.ttl -= 0.1
-            particle.speed_x += particle.speed_x /10
-            pygame.draw.circle(screen, (255, 255, 255), [int(particle.position_x), int(particle.position_y)], int(particle.ttl))
+            particle.speed_x += particle.velocity_x
+            particle.speed_y += particle.velocity_y
+            pygame.draw.circle(screen, (255, 255, 255), [int(particle.position_x), int(particle.position_y)], int(particle.size))
             if particle.ttl <= 0:
                 rem.append(particles.index(particle))
             
-        for to_rem in rem:
-            particles.pop(to_rem)
+        '''for to_rem in rem:
+            particles.pop(to_rem)'''
     
     # Buttons ------------------------------------------------ #
     for event in pygame.event.get():
