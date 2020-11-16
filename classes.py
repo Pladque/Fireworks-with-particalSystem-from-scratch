28b4b6e9af60da4c6810f4d69069a5e68ff5bfe5
Pladque <63751303+Pladque@users.noreply.github.com>
2020-11-16 21:06:02 +0100

    Firework class

--- classes.py
+++ classes.py
@@ -1,11 +1,44 @@
 from random import randint
+import pygame
 class Particle:
-    def __init__(self, position, speed, ttl, size):
-        self.position_x = position[0]
-        self.position_y = position[1]
+    def __init__(self, speed, ttl, size):
         self.speed_x = speed[0]
         self.speed_y = speed[1]
         self.ttl = ttl
         self.size = size
-        self.velocity_x = randint(-10,10) / 10000
-        self.velocity_y = randint(-10,10) / 10000
+        self.velocity_x = randint(-20,20) / 1000
+        self.velocity_y = randint(-20,20) / 1000
+
+class  Firework:
+
+    def __init__(self, particles, position, delay = 0):
+        self.shapes = [particles]
+        self.delay = delay
+        self.position_x = position[0]
+        self.position_y = position[1]
+
+        for shape in self.shapes:
+                rem = []
+                for particle in shape:
+                    particle.position_x = self.position_x
+                    particle.position_y = self.position_y 
+
+
+    def blowup(self, screen):
+        if self.delay <= 0:
+            for shape in self.shapes:
+                rem = []
+                for particle in shape:
+                    particle.position_x += particle.speed_x
+                    particle.position_y += particle.speed_y
+                    particle.ttl -= 0.1
+                    particle.speed_x += particle.velocity_x
+                    particle.speed_y += particle.velocity_y
+                    particle.ttl -= 0.1
+                    pygame.draw.circle(screen, (255, 255, 255), [int(particle.position_x), int(particle.position_y)], int(particle.size))
+                    if particle.ttl <= 0:
+                        rem.append(particle)
+
+                for to_rem in rem:
+                    shape.remove(to_rem)
+        else: self.delay -= 0.02
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import datetime
 # Setup pygame/window ---------------------------------------- #
 mainClock = pygame.time.Clock()
 from pygame.locals import *
-from classes import Particle
+from classes import Particle, Firework
 from brightness import get_bright
 pygame.init()
 WIDTH = HEIGHT = 800
@@ -24,33 +24,24 @@ do_it_once = True
 
 year = datetime.datetime.now().year
 hour = datetime.datetime.now().hour
+
+
+for _ in range(PARTICLES_DENSE):
+    particles.append(Particle([random.randint(-10,10) / 10000, random.randint(-10,10) / 10000], random.randint(10, 18), random.randint(1, 2)))
+
+fireworks = [Firework(particles,[WIDTH / 2 + random.randint(-10,10),HEIGHT / 2 + random.randint(-10,10)], 0.5)]
  
 # Loop ------------------------------------------------------- #
 while True:
     # Background --------------------------------------------- #
     screen.fill((0,0,get_bright(datetime.datetime.now().hour)))
+    
 
     #if datetime.datetime.now().year != year:
-    if True:
-        if do_it_once:
-            for _ in range(PARTICLES_DENSE):
-                particles.append(Particle([WIDTH / 2 + random.randint(-10,10),HEIGHT / 2 + random.randint(-10,10)], 
-                [random.randint(-10,10) / 10000, random.randint(-10,10) / 10000], random.randint(10, 18), random.randint(1, 2)))
-            do_it_once = False
-            
-        rem = []
-        for particle in particles:
-            particle.position_x += particle.speed_x
-            particle.position_y += particle.speed_y
-            particle.ttl -= 0.1
-            particle.speed_x += particle.velocity_x
-            particle.speed_y += particle.velocity_y
-            pygame.draw.circle(screen, (255, 255, 255), [int(particle.position_x), int(particle.position_y)], int(particle.size))
-            if particle.ttl <= 0:
-                rem.append(particles.index(particle))
-            
-        '''for to_rem in rem:
-            particles.pop(to_rem)'''
+    if True:     
+        for firework in fireworks:
+            firework.blowup(screen)
+        
     
     # Buttons ------------------------------------------------ #
     for event in pygame.event.get():
@@ -64,4 +55,4 @@ while True:
                 
     # Update ------------------------------------------------- #
     pygame.display.update()
-    mainClock.tick(60)
+    mainClock.tick(50)
